7dccf47a7aa5eed1c6470b3d794cfcbd04ee0252
klaegkev <kevin.klaeger@proton.me>
2025-03-30 16:15:07 +0200

    set colorscheme

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -19,11 +19,16 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import/override with your plugins
+    {
+      "LazyVim/LazyVim",
+      opts = {
+        colorscheme = "catppuccin-mocha",
+      },
+    },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
-    { "akinsho/toggleterm.nvim", version = "*", config = true },
     {
       "nvim-treesitter/nvim-treesitter",
       opts = {
